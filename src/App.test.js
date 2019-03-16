12dd028935d0699d69f56d1c8b0c069d2e5b9356
Latif Boussari <latifboussari@MacBook-Pro-de-Latif.local>
2019-03-16 11:20:13 +0100

    add dockerfile

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+
+
+
+
+
+
+// test:
+// build:
+//   context: .
+//   dockerfile: Dockerfile.dev
+// volumes:
+//   - .:/app
+//   - /app/node_modules
+// command: ["npm","run","test"]
